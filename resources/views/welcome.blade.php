b74cdf548c1cb8550206b2ba504e856dfe6ad526
Vinicios <viniciosking309@gmail.com>
2025-06-29 08:48:27 -0300

    other ajusts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-    @vite('resources/scss/app.scss')
+    {{-- @vite('resources/scss/app.scss') --}}
 
     <!-- Styles -->
     {{-- <style>
